56e8e5a0c4f20eb3b26ffda13f9bbb457937f125
Ankit Kumar Yadav <ankit2005pky@gmail.com>
2026-06-18 17:28:32 +0530

    Update baseURL to include '/api' path

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import  axios from 'axios'
 
 const api = axios.create({
-  baseURL: "https://eventora-backend-0pqn.onrender.com",
+  baseURL: "https://eventora-backend-0pqn.onrender.com/api",
   headers: {
     'Content-Type': 'application/json',
   },
